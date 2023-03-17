01645af160d7c7ec02f8e68267f62ad2b6890097
Ryan den Otter <ryan.denotter@realist.ai>
2023-03-17 11:18:29 -0700

    Adding the wheelhouse

--- package_linux.sh
+++ package_linux.sh
@@ -1,9 +1,11 @@
 # Build the CLI tooling
+WHEELHOUSE='wheelhouse_linux'
+
 python setup.py bdist_wheel --plat-name=manylinux1_x86_64
-cp dist/bigquery_python_utils-0.1-py3-none-manylinux1_x86_64.whl wheelhouse
+cp dist/bigquery_python_utils-0.1-py3-none-manylinux1_x86_64.whl ${WHEELHOUSE}
 rm -r dist
 rm -r build
 rm -r bigquery_python_utils.egg-info
 
 # Gather the dependencies to allow install on servers that don't have access to the internet
-pip download  --no-deps  --platform=manylinux_x86_64 --dest wheelhouse --no-cache -r requirements.txt
+pip download  --no-deps  --platform=manylinux_x86_64 --dest ${WHEELHOUSE} --no-cache -r requirements.txt
